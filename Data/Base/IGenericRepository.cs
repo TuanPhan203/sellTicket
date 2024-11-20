1c6cdeaf6ec431767b171ee306dc6300336b02d7
TuanPhan203 <phananhtuan1145@gmail.com>
2024-11-20 09:18:45 +0700

    Add viewmodel and viewbag, implement ViewDetail in controller for Movie

--- Data/Base/IGenericRepository.cs
+++ Data/Base/IGenericRepository.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace eTickets.Data.Base
@@ -7,7 +9,9 @@ namespace eTickets.Data.Base
     public interface IGenericRepository<T> where T: class,IEntityBase, new()
     {
         Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includeProperties);
         Task<T> GetByIdAsync(int id);
+        Task<T> GetByIdAsync(int id, params Expression<Func<T, object>>[] includeProperties);
         Task CreateAsync(T entity);
         Task UpdateAsync(int id, T entity);
         Task DeleteAsync(int id);
---
+++ Data/Services/MovieRepository.cs
@@ -0,0 +1,17 @@
+﻿using eTickets.Data.Base;
+using eTickets.Models;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services
+{
+    public class MovieRepository : GenericRepository<Movie>, IMovieRepository
+    {
+        public MovieRepository(TicketContext context) : base(context) 
+        {
+        }
+        public async Task AddNewMovieAsync()
+        {
+
+        }
+    }
+}
