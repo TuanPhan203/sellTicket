fd80bf4014cc9a72ef9882ddd6fcd749bf555123
TuanPhan203 <phananhtuan1145@gmail.com>
2024-11-28 11:20:59 +0700

    ShoppingCart AddItem, RemoveItem, GetShoppingCartItem

--- Models/ShoppingCartItem.cs
+++ Models/ShoppingCartItem.cs
@@ -1,6 +1,17 @@
-﻿namespace eTickets.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
 {
     public class ShoppingCartItem
     {
+
+        [Key]
+        public int Id { get; set; }
+
+        public Movie Movie { get; set; }
+        public int Amount { get; set; }
+
+
+        public string ShoppingCartId { get; set; }
     }
 }
